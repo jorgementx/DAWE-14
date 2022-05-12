04d474c51a7787fbfa6a01323a18929b782713dd
Jorge Mentxaka <50245342+jorgementx@users.noreply.github.com>
2022-05-12 21:36:04 +0200

    por lo visto no se habia subido mas que el commit inicial del proyecto

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({onClick, className = '', children}) =>
+    <button
+        onClick={onClick}
+        className={className}
+        type="button"
+    >{children}
+    </button>
